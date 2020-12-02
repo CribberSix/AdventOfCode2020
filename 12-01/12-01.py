461c2244c2c6203002223df8ada2e62b33f51a2c
CribberSix <seifert.victor@web.de>
2020-12-02 09:17:22 +0100

    performance enhancement - break as soon as a solution is foudn

--- 12-01/12-01.py
+++ 12-01/12-01.py
@@ -8,17 +8,25 @@ for x in input_cleaned:
     for y in input_cleaned:
         if x + y == 2020:
             print(f"Ergebnis ist {x * y} mit den Zahlen {x} und {y}.")
-
+            break
+    else:
+        continue
+    break
 
 # get input from file
 with open("input_part2.txt", "r") as f:
     input = list(f)
 input_cleaned = [int(x[:-1]) for x in input]
 
-
 for x in input_cleaned:
     for y in input_cleaned:
         for z in input_cleaned:
             if z + x + y == 2020:
                 print(f"Ergebnis ist {x * y * z} mit den Zahlen {x}, {y} und {z}.")
-
+                break
+        else:
+            continue
+        break
+    else:
+        continue
+    break
